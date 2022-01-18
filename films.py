a20c9b61be4f076610e18f8448226c1357e69e2d
Nichole Boothman <nbooth731@gmail.com>
2022-01-17 21:35:31 -0500

    cleaned

--- films.py
+++ films.py
@@ -28,3 +28,17 @@ def films_2000s():
     print(movies_after_2000)
 
 # films_2000s()
+
+
+missing_data = df.isnull().any()
+missing_locations = df['Locations'].isnull().value_counts()
+missing_fun_facts = df['Fun Facts'].isnull().value_counts()
+missing_production = df['Production Company'].isnull().value_counts()
+missing_distributor = df['Distributor'].isnull().value_counts()
+missing_writers = df['Writer'].isnull().value_counts()
+missing_actor1 = df['Actor 1'].isnull().value_counts()
+missing_actor2 = df['Actor 2'].isnull().value_counts()
+missing_actor3 = df['Actor 3'].isnull().value_counts()
+print(f'Location: {missing_locations}, Fun Facts: {missing_fun_facts}, Production: {missing_production}, Distributor: {missing_distributor}, Writer: {missing_writers}, Actor 1: {missing_actor1}, Actor 2: {missing_actor2}, Actor 3: {missing_actor3}')
+
+df.drop('Fun Facts', 1)
